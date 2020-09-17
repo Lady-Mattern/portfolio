c6d070268e005ac99edd3e958b190e739101fe10
Leah Mattern <ladyleahmattern@gmail.com>
2020-09-16 23:58:58 -0400

    Added some photos to the projects page. Updated styles

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ProjectCard from '../components/ProjectCard';
-import mustache from '../images/mustache.jpg'
+
+// Images
+import mustache from '../images/mustache.jpg';
+import mozart from '../images/mozart.png';
 
 class Projects extends Component {
     render () {
@@ -8,20 +11,21 @@ class Projects extends Component {
             <div className="Projects">
                 <h1>Here Are Some of My Favorite Projects</h1>
                 <div>
-                    <ProjectCard 
+                    {/* Cards commented out need to be finished before revealing */}
+                    {/* <ProjectCard 
                         title="REBA of Gem County"
                         image=""
                         site=""
                         languages=""
                         github=""
-                    />
-                    <ProjectCard 
+                    /> */}
+                    {/* <ProjectCard 
                         title="Lend A Note"
                         image=""
                         site=""
                         languages="Ruby on Rails, SQL, PostgresQL, Gatsby"
                         github=""
-                    />
+                    /> */}
                     <ProjectCard 
                         title="Paleo Pie"
                         image={mustache}
@@ -38,10 +42,10 @@ class Projects extends Component {
                     />
                     <ProjectCard 
                         title="Music With Mozart"
-                        image=""
-                        site=""
+                        image={mozart}
+                        site="https://tender-golick-79088e.netlify.app/"
                         languages="HTML, CSS, JavaScript"
-                        github=""
+                        github="https://github.com/Lady-Mattern/GA_Unit1_Project"
                     />
                 </div>
             </div>
